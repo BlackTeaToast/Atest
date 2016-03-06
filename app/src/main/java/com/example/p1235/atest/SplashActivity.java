e76e576db7c3bc5b0db14d5f32bd9ce2ed329cb6
Chien-Hong Liao <p12355668@gmail.com>
2016-03-06 13:17:55 +0800

    Test2

--- app/src/main/java/com/example/p1235/atest/SplashActivity.java
+++ app/src/main/java/com/example/p1235/atest/SplashActivity.java
@@ -1,19 +1,49 @@
 package com.example.p1235.atest;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.p1235.atest.MainActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 3000; //開啟畫面時間(3秒)
+    private RelativeLayout relativeLayout;
+    private TextView bottomText;
+
+
+    public void init()
+    {
+        relativeLayout = (RelativeLayout) findViewById(R.id.layout1);
+        bottomText = (TextView) findViewById(R.id.textView1);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        init();
+        new Handler().postDelayed(new Runnable() {
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class); //MainActivity為主要檔案名稱
+                startActivity(intent);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
